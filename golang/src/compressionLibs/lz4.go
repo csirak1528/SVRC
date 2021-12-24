3a6ad73a1cc115044fceefbe13214d8dae0ff1db
calebs-py <calebsirak@gmail.com>
2021-12-23 22:43:31 -0500

    feat:updated algos

--- golang/src/compressionLibs/lz4.go
+++ golang/src/compressionLibs/lz4.go
@@ -1,11 +1,32 @@
 package compressionLibs
 
-var LZ4 = CompressionAlgorithm{}
+import (
+	"fmt"
+	"os"
 
-func Compress() {
+	"github.com/pierrec/lz4"
+)
+
+func CompressLZ4(data []byte) {
+	buf := make([]byte, len(data))
+	n, err := lz4.CompressBlockHC(data, buf, 10000)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if n >= len(data) {
+		fmt.Println("is not compressible")
+	}
+	fmt.Println(float64(len(data)) / float64(n))
+	// Allocated a very large buffer for decompression.
 
 }
 
+const TESTPATH = "testfile.png"
+
 func GetBenchmarksLZ4() map[string]interface{} {
+	testBytes, err := os.ReadFile(TESTPATH)
+	Check(err)
+	CompressLZ4(testBytes)
+
 	return map[string]interface{}{}
 }
